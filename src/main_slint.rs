48b71692b3a3bf412abce9d36a0598fbd83ed70a
Danila Bain <danila.bain@yandex.com>
2025-03-27 12:25:44 +0300

    BitSelector ui component

--- src/main_slint.rs
+++ src/main_slint.rs
@@ -9,13 +9,6 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     let physical_size = logical_size.to_physical(ui.window().scale_factor());
     ui.window().set_size(physical_size); // don't wait for "Set Size" to be clicked; set the size now!
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
 
     ui.run()?;
 
